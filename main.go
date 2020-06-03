9798e6850ba4cd1cfcda01bb138478a6af56c514
Matan Budimir <matanbudimir@gmail.com>
2020-06-03 15:39:23 +0200

    Go Martini framework is now supported

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package connect
 
 import (
 	"connect-go/handle"
+	"github.com/go-martini/martini"
 	"net/http"
 )
 
@@ -9,4 +10,13 @@ import (
 func URL() {
 	http.HandleFunc("/connect/login", Handle.Login)
 	http.HandleFunc("/connect/install", Handle.Env)
+}
+
+// MartiniURL function returns url to marting application routes
+func MartiniURL(m martini.ClassicMartini) {
+	m.Group("/connect", func (r martini.Router) {
+		r.Get("/connect/login", Handle.Login)
+		r.Post("/connect/install", Handle.Env)
+		r.Get("/connect/install", Handle.Env)
+	})
 }
